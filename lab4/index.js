4a8b5a1d2958a99655e7d191ac5c5a501230e79e
kazukiii <kazumatsu.dev28@gmail.com>
2023-05-12 23:14:35 -0700

    add lab4

---
+++ lab4/index.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+/* ---------------------------- setups and config --------------------------- */
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+/* ------------------------------- middleware ------------------------------- */
+app.use('/api/members', require('./routes/members'))
+
+app.get('/', (req, res) => res.render('home', { msg: 'Health Check' }))
+
+app.use((req, res) => res.sendFile(path.join(__dirname, 'public', '404.html')))
+
+/* -------------------------------- listener -------------------------------- */
+const PORT = process.env.PORT || 8000
+app.listen(PORT)
